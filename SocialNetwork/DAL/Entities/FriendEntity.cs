72e7ed5276b6217d8066b5bb3fbf6205b90f472f
kema1989 <86516029+kema1989@users.noreply.github.com>
2022-01-14 19:47:04 +0600

    DAL: Entities2

---
+++ SocialNetwork/DAL/Entities/FriendEntity.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DAL.Entities
+{
+    class FriendEntity
+    {
+        public int id { get; set; }
+        public int userId { get; set; }
+        public int friendId { get; set; }
+    }
+}
---
+++ SocialNetwork/DAL/Entities/MessageEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.DAL.Entities
+{
+    public class MessageEntity
+    {
+        public int id { get; set; }
+        public string content { get; set; }
+        public int senderId { get; set; }
+        public int recipientId { get; set; }
+    }
+}
