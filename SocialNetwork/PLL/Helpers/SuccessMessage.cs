4e10e7eb0cb9d432e59d49cf67077a4df332d140
kema1989 <86516029+kema1989@users.noreply.github.com>
2022-01-19 22:06:00 +0600

    added pll

---
+++ SocialNetwork/PLL/Helpers/SuccessMessage.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SocialNetwork.PLL.Helpers
+{
+    public static class SuccessMessage
+    {
+        public static void Show(string message)
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine(message);
+            Console.ResetColor();
+        }
+    }
+}
